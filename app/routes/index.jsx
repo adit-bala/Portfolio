3cb8361c5a658a0921ce6e69ae8f879e610a9e4b
adit-bala <adityarbala@gmail.com>
2022-11-22 23:17:36 -0800

    style bio; init nav html

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,12 +2,35 @@ import pfp from "../../public/Aditya-Balasubramanian.jpg";
 
 export default function Index() {
   return (
-  <div class="py-8 max-w-sm mx-auto rounded-xl items-center space-x-4">
-    <div class="shrink-0">
-      <img src={pfp} class="h-12 w-12" alt="Adit"></img>
-    </div>
-    <div class="text-xl font-medium text-black">Aditya Balasubramanian</div>
-    <p class="text-slate-5000"> Greetings! I'm a student at UC Berkeley studying Computer Science. Welcome to my corner of the internet where I share some personal experiences and thoughts!</p>
-  </div>
+    <>
+      <nav className="">
+        <div className="">
+          <div class="">
+            <ul>
+              <li>
+                <a href="/"> Home </a>
+              </li>
+              <li>
+                <a href="/"> Blog </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="py-10 max-w-sm mx-auto items-center space-x-4 space-y-3 text-center">
+        <div className="shrink-0 flex justify-center">
+          <img src={pfp} className="md:h md:w rounded-full" alt="Adit"></img>
+        </div>
+        <div className="text-2xl font-semibold text-black">
+          Aditya Balasubramanian
+        </div>
+        <p className="text-slate-5000">
+          {" "}
+          Greetings! I'm a student at UC Berkeley studying Computer Science.
+          Welcome into my little corner of the internet where I house some of my
+          musings!{" "}
+        </p>
+      </div>
+    </>
   );
 }
