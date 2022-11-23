44c0f43a8311f85f0d86733babd71af5c8680ef0
adit-bala <adityarbala@gmail.com>
2022-11-22 18:23:37 -0800

    add base html

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,13 @@
+import pfp from "../../public/Aditya-Balasubramanian.jpg";
+
 export default function Index() {
   return (
-    <h1 className="text-3xl font-bold underline"> The start... </h1> 
+  <div class="py-8 max-w-sm mx-auto rounded-xl items-center space-x-4">
+    <div class="shrink-0">
+      <img src={pfp} class="h-12 w-12" alt="Adit"></img>
+    </div>
+    <div class="text-xl font-medium text-black">Aditya Balasubramanian</div>
+    <p class="text-slate-5000"> Greetings! I'm a student at UC Berkeley studying Computer Science. Welcome to my corner of the internet where I share some personal experiences and thoughts!</p>
+  </div>
   );
 }
